9c943ebae9327976062f672f08a80529b37da174
sna_lodosha <measna.ly@lodosha.jp>
2024-01-27 14:32:27 -0600

    fix issue

--- public/render-page.js
+++ public/render-page.js
@@ -14095,24 +14095,24 @@ function v(){return(v=Object.assign||function(e){for(var t=1;t<arguments.length;
 /***/ }),
 
 /***/ "react-dom/server":
-/*!*********************************************************************************!*\
-  !*** external "/Users/measna/Desktop/v4-main/node_modules/react-dom/server.js" ***!
-  \*********************************************************************************/
+/*!******************************************************************************************************!*\
+  !*** external "/Users/measna/Documents/Personal Project/portfolio/node_modules/react-dom/server.js" ***!
+  \******************************************************************************************************/
 /***/ ((module) => {
 
 "use strict";
-module.exports = require("/Users/measna/Desktop/v4-main/node_modules/react-dom/server.js");;
+module.exports = require("/Users/measna/Documents/Personal Project/portfolio/node_modules/react-dom/server.js");;
 
 /***/ }),
 
 /***/ "react":
-/*!****************************************************************************!*\
-  !*** external "/Users/measna/Desktop/v4-main/node_modules/react/index.js" ***!
-  \****************************************************************************/
+/*!*************************************************************************************************!*\
+  !*** external "/Users/measna/Documents/Personal Project/portfolio/node_modules/react/index.js" ***!
+  \*************************************************************************************************/
 /***/ ((module) => {
 
 "use strict";
-module.exports = require("/Users/measna/Desktop/v4-main/node_modules/react/index.js");;
+module.exports = require("/Users/measna/Documents/Personal Project/portfolio/node_modules/react/index.js");;
 
 /***/ }),
 
