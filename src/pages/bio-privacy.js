00740cee4f809bdaaeca0bdab7ce4735d287dd61
measna_lazi <lymeasna1@gmail.com>
2026-01-29 13:54:52 -0500

    remove style of bio privacy

--- src/pages/bio-privacy.js
+++ src/pages/bio-privacy.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { navDelay } from '@utils';
 import { Layout } from '@components';
 import { usePrefersReducedMotion } from '@hooks';
@@ -20,7 +20,7 @@ const StyledContent = styled.div`
   h1 {
     font-size: clamp(30px, 5vw, 50px);
     font-weight: 400;
-    padding-top: 150px;
+    padding-top: 50px;
     margin-bottom: 40px;
     text-align: center;
   }
@@ -45,55 +45,72 @@ const StyledContent = styled.div`
     }
   }
 
+
+`;
+
+const PrivacyGlobalStyle = createGlobalStyle`
+  body {
+    background-color: #ffffff !important;
+    color: #111111 !important;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    color: #000000 !important;
+  }
   a {
-    ${({ theme }) => theme.mixins.inlineLink};
+    color: #0000EE !important;
+    text-decoration: underline !important;
+  }
+  a {
+    color: #0000EE !important;
+    text-decoration: underline !important;
   }
 `;
 
 const BioPrivacy = ({ data, location }) => {
-    const [isMounted, setIsMounted] = useState(false);
-    const prefersReducedMotion = usePrefersReducedMotion();
+  const [isMounted, setIsMounted] = useState(false);
+  const prefersReducedMotion = usePrefersReducedMotion();
 
-    useEffect(() => {
-        if (prefersReducedMotion) {
-            return;
-        }
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
 
-        const timeout = setTimeout(() => setIsMounted(true), navDelay);
-        return () => clearTimeout(timeout);
-    }, []);
+    const timeout = setTimeout(() => setIsMounted(true), navDelay);
+    return () => clearTimeout(timeout);
+  }, []);
 
-    const { html, frontmatter } = data.markdownRemark;
-    const { title } = frontmatter;
+  const { html, frontmatter } = data.markdownRemark;
+  const { title } = frontmatter;
 
-    const content = (
-        <StyledMainContainer className="fillHeight">
-            <StyledContent dangerouslySetInnerHTML={{ __html: html }} />
-        </StyledMainContainer>
-    );
+  const content = (
+    <StyledMainContainer className="fillHeight">
+      <StyledContent dangerouslySetInnerHTML={{ __html: html }} />
+    </StyledMainContainer>
+  );
 
-    return (
-        <Layout location={location} minimal>
-            <Helmet title={title} />
+  return (
+    <Layout location={location} minimal>
+      <PrivacyGlobalStyle />
+      <Helmet title={title} />
 
-            {prefersReducedMotion ? (
-                <>{content}</>
-            ) : (
-                <TransitionGroup component={null}>
-                    {isMounted && (
-                        <CSSTransition timeout={500} classNames="fadeup">
-                            {content}
-                        </CSSTransition>
-                    )}
-                </TransitionGroup>
-            )}
-        </Layout>
-    );
+      {prefersReducedMotion ? (
+        <>{content}</>
+      ) : (
+        <TransitionGroup component={null}>
+          {isMounted && (
+            <CSSTransition timeout={500} classNames="fadeup">
+              {content}
+            </CSSTransition>
+          )}
+        </TransitionGroup>
+      )}
+    </Layout>
+  );
 };
 
 BioPrivacy.propTypes = {
-    data: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
 };
 
 export default BioPrivacy;
